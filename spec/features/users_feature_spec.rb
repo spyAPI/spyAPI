58c42bf47a271d4e2b34a02d6445316fe11b4d5e
Sophie Gill <sophie-gill@hotmail.com>
2016-08-30 15:14:21 +0100

    adds tests for user authentication

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -14,4 +14,29 @@ feature 'User can sign in and out' do
     end
 
   end
+
+
+  context 'User signed in on the homepage' do
+    before do
+      visit '/'
+      click_link 'Sign up'
+      fill_in 'Email', with: "test@example.com"
+      fill_in 'Password', with: "secret"
+      fill_in 'Password confirmation', with: "secret"
+      click_button 'Sign up'
+    end
+
+    it 'should see Sign out link' do
+      visit '/'
+      expect(page).to(have_link("Sign out"))
+    end
+
+    it 'should not see a Sign in or Sign up link' do
+      visit '/'
+      expect(page).not_to(have_link("Sign up"))
+      expect(page).not_to(have_link("Sign in"))
+    end
+
+  end
+
 end
