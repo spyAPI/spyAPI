ff76f0dff18a85f2f94828427a5b28c3fdcf3d39
Jack Oddy <jack.oddy91@gmail.com>
2016-08-30 16:22:32 +0100

    adds test to definitely make sure JSONs are being selected through the params

--- spec/features/API_spec.rb
+++ spec/features/API_spec.rb
@@ -18,4 +18,16 @@ feature 'spyAPI' do
       expect(page).not_to have_content('{"message":"No API Key found in headers, body or querystring"}')
     end
   end
+  context 'API key is given' do
+    before do
+      api = Api.create(key: 'abcdefghijklmnopqrstuvwxyz0123456789')
+      Json.create(name: 'test-data', content: "Hello, World!", api_id: api.id)
+      Json.create(name: 'test-data2', content: "Hello, World, again!", api_id: api.id)
+    end
+    scenario 'specific JSON is returned' do
+      visit '/api?api-key=abcdefghijklmnopqrstuvwxyz0123456789&json=test-data2'
+      expect(page).to have_content('Hello, World, again!')
+      expect(page).not_to have_content('{"message":"No API Key found in headers, body or querystring"}')
+    end
+  end
 end
