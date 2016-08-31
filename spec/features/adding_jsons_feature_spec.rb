c4b1afd0cbb31f7b30d3c39e8d35b6ff4302ba76
lukew244 <luke.webster86@gmail.com>
2016-08-31 11:31:26 +0100

    MVP MVP MVP

--- spec/features/adding_jsons_feature_spec.rb
+++ spec/features/adding_jsons_feature_spec.rb
@@ -14,9 +14,18 @@ feature 'adding JSONs' do
       expect(page).to have_content 'No JSONs yet'
       expect(page).to have_link 'Add JSON'
     end
-
   end
 
+  context 'creating JSONs' do
+    scenario 'user can create a JSON' do
+      visit '/'
+      click_link 'Test API'
+      click_link 'Add JSON'
+      fill_in 'Name', with: 'Test-data'
+      fill_in 'Content', with: '{"message":"hello world"}'
+      click_button 'Create Json'
+    end
+  end
 
 
 end
