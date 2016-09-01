29663c8194b5bd3d3c3990cab7e7dc1b9bb87d4f
Sophie Gill <sophie-gill@hotmail.com>
2016-09-01 14:16:35 +0100

    adds validation of json name

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -13,15 +13,15 @@ def sign_out
   click_link 'Sign out'
 end
 
-def add_api
+def add_api(name: 'Test API')
   click_link 'add API'
-  fill_in 'Name', with: 'Test API'
+  fill_in 'Name', with: name
   click_button 'Create Api'
 end
 
-def add_JSON(content: '{"message":"hello world"}')
+def add_JSON(content: '{"message":"hello world"}', link: 'Test API')
   visit '/'
-  click_link 'Test API'
+  click_link link
   click_link 'Add JSON'
   fill_in 'Name', with: 'Test-data'
   fill_in 'Content', with: content
