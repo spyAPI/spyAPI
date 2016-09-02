6483c68f504f160698532c63a8c881ce9318113c
Jack Oddy <jack.oddy91@gmail.com>
2016-09-02 09:07:54 +0100

    makes all test pass and fixes some stuff

--- app/models/json.rb
+++ app/models/json.rb
@@ -1,3 +1,14 @@
 class Json < ApplicationRecord
   belongs_to :api
 end
+
+#we know this shouldnt be here ¯\(°_o)/¯
+class String
+  def is_json?
+    begin
+      !!JSON.parse(self)
+    rescue
+      false
+    end
+  end
+end
